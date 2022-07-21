000325b4a9e07b83df4aabb0f2962f8888a7f236
JHONASPRILLA <jhonarroz9@gmial.com>
2022-07-21 08:11:14 -0500

    esta es la clase 3

---
+++ Clase3/clase3.js
@@ -0,0 +1,11 @@
+alert ("holi bby");
+
+let array=[];
+
+console.log(array);
+
+let match=["layker vs warriors","boston vs thunder","pelicans vs bulls","hornet vs broklyn"];
+let shoes=["nike","adidas","vans","reebook","converse","under armor"];
+let number=[23,24,0,1,00,7,10,11];
+let name=["carlos","juan","camilo","jhon","tailor","roberth","jhonier"];
+console.table (match,shoes,number,name)
